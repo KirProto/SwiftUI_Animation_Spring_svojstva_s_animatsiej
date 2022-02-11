33228b2f4b38fa397120085f7c458b71b0cff8c2
Kir Proto Q <kir.proto@ya.ru>
2022-02-11 10:04:30 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Animation_Spring_svojstva_s_animatsiej/Yan_SwiftUI_Animation_Spring_svojstva_s_animatsiejApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_Animation_Spring_svojstva_s_animatsiejApp.swift
+//  Yan_SwiftUI_Animation_Spring_svojstva_s_animatsiej
+//
+//  Created by KIR Q on 2022.02.11.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_Animation_Spring_svojstva_s_animatsiejApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
